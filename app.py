62321f275c8fe7e7fba48c2ffd0147439c9b473d
DougAF <31972099+DougAF@users.noreply.github.com>
2019-07-11 14:06:49 -0500

    images for methods added, mldata route working, csv col name fixed

--- app.py
+++ app.py
@@ -15,21 +15,17 @@ from flask import Flask, jsonify, render_template
 
 # Create Engine
 engine = create_engine("sqlite:///cities_db_3.sqlite")
-engine2 = create_engine("sqlite:///industries_db.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base() # AUTO MAP OR DECLARATIVE?
-Base2 = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
-Base2.prepare(engine2, reflect=True)
 
 # Save references to table 'cities'
 Cities = Base.classes.cities
-# Industry = Base2.classes.industry
+Industry = Base.classes.industry
 # Create our session (link) from Python to the DB
 session = Session(engine)
-session2 = Session(engine2)
 
 #################################################
 # Flask Setup
@@ -136,13 +132,13 @@ def city_metadata():
 
 @app.route("/mldata")
 def ml_metadata():
-    engine = create_engine("sqlite:///industries_db.sqlite")
+    engine = create_engine("sqlite:///cities_db_3.sqlite")
 # reflect an existing database into a new model
     Base = automap_base() # AUTO MAP OR DECLARATIVE?
 # reflect the tables
     Base.prepare(engine, reflect=True)
 # Save references to table
-    Industry = Base.classes.cities
+    Industry = Base.classes.industry
 # Create our session (link) from Python to the DB
     session = Session(engine)
 
@@ -174,7 +170,8 @@ def ml_metadata():
         Industry.health_social_assistance_ratio,
         Industry.arts_entertainment_and_recreation_ratio,
         Industry.accommodation_food_services_ratio,
-        Industry.other_ratio,
+        #Other_ratio col bugged
+       # Industry.other_ratio,
         Industry.industries_not_classified_ratio,
         Industry.commute_time_per_person
     ]
@@ -191,38 +188,39 @@ def ml_metadata():
         ml_metadata_dict = {}
         ml_metadata_dict["city_state"] = result[0]
         ml_metadata_dict["houses_total"] = result[1]
-        ml_metadata_dict["pop_18_to_24_total"] = [result[2], result[3]]
-        ml_metadata_dict["renters_total"] = result[4]
-        ml_metadata_dict["house_units"] = result[5]
-        ml_metadata_dict["population"] = result[6]
-        ml_metadata_dict["establishments_total"] = result[7]
-        ml_metadata_dict["year"] = result[8]
-        ml_metadata_dict["biz_gagriculture_forest_fish_hunt_ratiorowth_Y"] = result[9]
-        ml_metadata_dict["mine_quarry_oil_gas_extraction_ratio"] = result[10]
-        ml_metadata_dict["utilities_ratio"] = result[11]
-        ml_metadata_dict["construction_ratio"] = result[12]
-        ml_metadata_dict["manufacturing_ratio"] = result[13]
-        ml_metadata_dict["wholesale_trade_ratio"] = result[14]
-        ml_metadata_dict["retail_ratio"] = result[15]
-        ml_metadata_dict["transport_warehousing_ratio"] = result[16]
-        ml_metadata_dict["information_ratio"] = result[17]
-        ml_metadata_dict["finance_insurance_ratio"] = result[18]
-        ml_metadata_dict["realestate_rent_lease_ratio"] = result[19]
-        ml_metadata_dict["professional_scientific_tech_services_ratio"] = result[20]
-        ml_metadata_dict["mgmt_companies_enterprises_ratio"] = result[21]
-        ml_metadata_dict["admin_support_waste_management_services_ratio"] = result[22]
-        ml_metadata_dict["educational_services_ratio"] = result[23]
-        ml_metadata_dict["health_social_assistance_ratio"] = result[24]
-        ml_metadata_dict["arts_entertainment_and_recreation_ratio"] = result[25]
-        ml_metadata_dict["accommodation_food_services_ratio"] = result[26]
-        ml_metadata_dict["other_ratio"] = result[27]
-        ml_metadata_dict["industries_not_classified_ratio"] = result[28]
-        ml_metadata_dict["commute_time_per_person"] = result[29]
+        ml_metadata_dict["pop_18_to_24_total"] = result[2]
+        ml_metadata_dict["renters_total"] = result[3]
+        ml_metadata_dict["house_units"] = result[4]
+        ml_metadata_dict["population"] = result[5]
+        ml_metadata_dict["establishments_total"] = result[6]
+        ml_metadata_dict["year"] = result[7]
+        ml_metadata_dict["biz_gagriculture_forest_fish_hunt_ratiorowth_Y"] = result[8]
+        ml_metadata_dict["mine_quarry_oil_gas_extraction_ratio"] = result[9]
+        ml_metadata_dict["utilities_ratio"] = result[10]
+        ml_metadata_dict["construction_ratio"] = result[11]
+        ml_metadata_dict["manufacturing_ratio"] = result[12]
+        ml_metadata_dict["wholesale_trade_ratio"] = result[13]
+        ml_metadata_dict["retail_ratio"] = result[14]
+        ml_metadata_dict["transport_warehousing_ratio"] = result[15]
+        ml_metadata_dict["information_ratio"] = result[16]
+        ml_metadata_dict["finance_insurance_ratio"] = result[17]
+        ml_metadata_dict["realestate_rent_lease_ratio"] = result[18]
+        ml_metadata_dict["professional_scientific_tech_services_ratio"] = result[19]
+        ml_metadata_dict["mgmt_companies_enterprises_ratio"] = result[20]
+        ml_metadata_dict["admin_support_waste_management_services_ratio"] = result[21]
+        ml_metadata_dict["educational_services_ratio"] = result[22]
+        ml_metadata_dict["health_social_assistance_ratio"] = result[23]
+        ml_metadata_dict["arts_entertainment_and_recreation_ratio"] = result[24]
+        ml_metadata_dict["accommodation_food_services_ratio"] = result[25]
+        #other_ratio col bugged 
+       # ml_metadata_dict["other_ratio"] = result[26]
+        ml_metadata_dict["industries_not_classified_ratio"] = result[26]
+        ml_metadata_dict["commute_time_per_person"] = result[27]
         
     #INSERT ADDITIONAL VARS HERE 
         ml_list.append(ml_metadata_dict)
         #ALLOWS ACCES TO ALL DATA BY DICT.KEY FOR MAPPING LEAFLET
-    meta_dict = {"keys": ml_list}
+    ml_dict = {"keys": ml_list}
     return jsonify(ml_dict)
 
 if __name__ == "__main__":
